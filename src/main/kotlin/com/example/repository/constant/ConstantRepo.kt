6f73706018926d3654f6e3b00e4a38c984cddcfb
Ananda Muhamad Lukman <88027328+anandalukman33@users.noreply.github.com>
2023-09-25 16:36:02 +0700

    Create All Heroes Endpoint Part 2

---
+++ src/main/kotlin/com/example/repository/constant/ConstantRepo.kt
@@ -0,0 +1,8 @@
+package com.example.repository.constant
+
+object ConstantRepo {
+    const val NEXT_PAGE_KEY = "nextPage"
+
+    const val PREVIOUS_PAGE_KEY = "prevPage"
+
+}
--- src/main/kotlin/com/example/routes/AllHeroes.kt
+++ src/main/kotlin/com/example/routes/AllHeroes.kt
@@ -1,18 +1,26 @@
 package com.example.routes
 
 import com.example.model.ApiResponse
+import com.example.repository.HeroRepository
 import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import org.koin.ktor.ext.inject
 
 fun Route.getAllHeroes() {
+
+    val heroRepository: HeroRepository by inject()
+
     get("/boruto/heroes") {
 
         try {
             val page = call.request.queryParameters["page"]?.toInt() ?: 1
             require(page in 1..5)
-            call.respond(message = page)
+
+            val apiResponse = heroRepository.getAllHeroes(page = page)
+            call.respond(message = apiResponse, status = HttpStatusCode.OK)
+
         } catch (e: NumberFormatException) {
             call.respond(
                 message = ApiResponse(success = false, message = "Only Number Allowed."),
@@ -21,7 +29,7 @@ fun Route.getAllHeroes() {
         } catch (e: IllegalArgumentException) {
             call.respond(
                 message = ApiResponse(success = false, message = "Heroes not Found."),
-                status = HttpStatusCode.BadRequest
+                status = HttpStatusCode.NotFound
             )
         }
 
