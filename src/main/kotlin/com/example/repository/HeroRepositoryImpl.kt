8bdc5359ed7134ec2c15007430483dc61f8eb3eb
Ananda Muhamad Lukman <88027328+anandalukman33@users.noreply.github.com>
2023-09-25 13:35:55 +0700

    Create All Heroes Endpoint Part 1

--- src/main/kotlin/com/example/repository/HeroRepositoryImpl.kt
+++ src/main/kotlin/com/example/repository/HeroRepositoryImpl.kt
@@ -4,12 +4,12 @@ import com.example.model.ApiResponse
 import com.example.model.Hero
 
 class HeroRepositoryImpl(
-    override val heroes: Map<Int, List<Hero>>,
-    override val page1: List<Hero>,
-    override val page2: List<Hero>,
-    override val page3: List<Hero>,
-    override val page4: List<Hero>,
-    override val page5: List<Hero>
+    override val heroes: Map<Int, List<Hero>> = emptyMap(),
+    override val page1: List<Hero> = emptyList(),
+    override val page2: List<Hero> = emptyList(),
+    override val page3: List<Hero> = emptyList(),
+    override val page4: List<Hero> = emptyList(),
+    override val page5: List<Hero> = emptyList()
 ) : HeroRepository {
     override suspend fun getAllHeroes(page: Int): ApiResponse {
         TODO("Not yet implemented")
