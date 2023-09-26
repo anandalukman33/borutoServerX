41eaad4138997b042eed442e8802e65037fa5159
Ananda Muhamad Lukman <88027328+anandalukman33@users.noreply.github.com>
2023-09-26 13:38:08 +0700

    Create Root Test Application

--- src/test/kotlin/com/example/ApplicationTest.kt
+++ src/test/kotlin/com/example/ApplicationTest.kt
@@ -2,10 +2,12 @@ package com.example
 
 import com.example.plugins.configureRouting
 import io.ktor.client.request.*
+import io.ktor.client.statement.*
 import io.ktor.http.*
 import io.ktor.server.testing.*
 import kotlin.test.Test
 import kotlin.test.assertEquals
+
 
 class ApplicationTest {
     @Test
@@ -14,8 +16,14 @@ class ApplicationTest {
             configureRouting()
         }
         client.get("/").apply {
-            assertEquals(HttpStatusCode.OK, status)
-            //assertEquals("Hello World!", bodyAsText())
+            assertEquals(
+                expected = HttpStatusCode.OK,
+                actual = status
+            )
+            assertEquals(
+                expected = "Welcome to Boruto API!",
+                actual = bodyAsText()
+            )
         }
     }
 }
