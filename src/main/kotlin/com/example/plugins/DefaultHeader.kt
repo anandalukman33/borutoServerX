29a7e9c0abcf49caaab916c1ae6a1982c311c6c3
Ananda Muhamad Lukman <88027328+anandalukman33@users.noreply.github.com>
2023-10-02 11:05:18 +0700

    Memberikan penjelasan pada DefaultHeader sebagai caching header untuk klien http

--- src/main/kotlin/com/example/plugins/DefaultHeader.kt
+++ src/main/kotlin/com/example/plugins/DefaultHeader.kt
@@ -5,6 +5,12 @@ import io.ktor.server.application.*
 import io.ktor.server.plugins.defaultheaders.*
 import java.time.Duration
 
+/**
+ * Default Header disini berfungsi sebagai caching
+ * penjelasannya adalah pada parameter header yaitu nilai/value yang digunakan untuk mengaktifkan caching gambar
+ * pada klien http secara otomatis pada tenggat waktu 365 hari
+ */
+
 fun Application.configureDefaultHeader() {
     install(DefaultHeaders) {
         val oneYearInSeconds = Duration.ofDays(365).seconds
